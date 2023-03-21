3234d32c6fce63cb625f1e1c58f1d93cea0abc2d
Richard Davenport <rad22684@gmail.com>
2023-03-21 19:09:14 +0000

    Remove base theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,22 +5,19 @@
 	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
 				<strong class="text-xl uppercase">Skeleton</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a class="btn btn-sm variant-ghost-surface" href="/"> Home </a>
-				<a class="btn btn-sm variant-ghost-surface" href="/super"> SuperForms </a>
-				<a class="btn btn-sm variant-ghost-surface" href="/table" > Table </a>
+				<a href="/"> Home </a>
+				<a href="/super"> SuperForms </a>
+				<a href="/table" > Table </a>
 				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
 	<slot />
 </AppShell>
